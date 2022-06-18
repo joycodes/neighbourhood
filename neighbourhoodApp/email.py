eb4e2b7cbe732e1ea2bdbc7597613c6e538abba9
joycodes <jnguttu@gmail.com>
2022-06-19 02:44:56 +0300

    configure email

---
+++ neighbourhoodApp/email.py
@@ -0,0 +1,29 @@
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+
+def send_signup_email_admin(name,receiver):
+    # Creating message subject and sender
+    subject = 'Welcome to MyNeighbourhood'
+    sender = 'joynguttu@gmail.com'
+
+    #passing in the context variables
+    text_content = render_to_string('email/email-admin.txt',{"name": name})
+    html_content = render_to_string('email/email-admin.html',{"name": name})
+
+    msg = EmailMultiAlternatives(subject,text_content,sender,[receiver])
+    msg.attach_alternative(html_content,'text/html')
+    msg.send()
+
+
+def send_signup_email_resident(name,username,password,admin,hood,receiver):
+    # Creating message subject and sender
+    subject = 'Welcome to MyNeighbourhood'
+    sender = 'joynguttu@gmail.com'
+
+    #passing in the context vairables
+    text_content = render_to_string('email/email-occupant.txt',{"name": name, "username":username, "password":password, "admin":admin, "hood":hood})
+    html_content = render_to_string('email/email-occupant.html',{"name": name, "username":username, "password":password, "admin":admin, "hood":hood})
+
+    msg = EmailMultiAlternatives(subject,text_content,sender,[receiver])
+    msg.attach_alternative(html_content,'text/html')
+    msg.send()
