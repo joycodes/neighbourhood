33ed348de2aa4e0320aa889df2d5c7a763b42562
joycodes <jnguttu@gmail.com>
2022-06-19 02:22:16 +0300

    configure mapbox

--- neighbourhoodApp/tests.py
+++ neighbourhoodApp/tests.py
@@ -4,9 +4,9 @@ from .models import Admin, Amenity, Post, Neighbourhood, Occupant, Business
 
 class AdminTestClass(TestCase):
     def setUp(self):
-        self.lorna = User(username = "lorna", email = "lorna@gmail.com",password = "1234")
-        self.admin = Admin(user= self.lorna)
-        self.lorna.save()
+        self.joy = User(username = "joy", email = "joy@gmail.com",password = "1234")
+        self.admin = Admin(user= self.joy)
+        self.joy.save()
         self.admin.save()
 
     def tearDown(self):
@@ -14,14 +14,14 @@ class AdminTestClass(TestCase):
         User.objects.all().delete()
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.lorna, User))
+        self.assertTrue(isinstance(self.joy, User))
         self.assertTrue(isinstance(self.admin, Admin))
 
 class OccupantTestClass(TestCase):
     def setUp(self):
-        self.lorna = User(username = "lorna", email = "lorna@gmail.com",password = "1234")
-        self.occupant = Occupant(user= self.lorna, profile_pic='mepng')
-        self.lorna.save()
+        self.joy = User(username = "joy", email = "joy@gmail.com",password = "1234")
+        self.occupant = Occupant(user= self.joy, profile_pic='mepng')
+        self.joy.save()
         self.occupant.save()
 
     def tearDown(self):
@@ -29,8 +29,7 @@ class OccupantTestClass(TestCase):
         User.objects.all().delete()
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.lorna, User))
+        self.assertTrue(isinstance(self.joy, User))
         self.assertTrue(isinstance(self.occupant, Occupant))
 
-class NeighbourhoodTestClass(TestCase):
-    
+# class NeighbourhoodTestClass(TestCase):
